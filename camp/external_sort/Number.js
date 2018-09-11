c7121154b995a496f31808c58a51d53a804eb925
secreter <1979510177@qq.com>
2018-09-11 19:34:02 +0800

    custom stream generate number

---
+++ camp/external_sort/Number.js
@@ -0,0 +1,33 @@
+/**
+ * Created by pengchaoyang on 2018/9/11
+ */
+/*
+自定义一个生成0-255数字的可读流
+ */
+const stream = require('stream')
+class RandomStream extends stream.Readable {
+    constructor (options={}) {
+        super(options)             // 传递参数
+        this.count=options.count||10
+
+    }
+    // 实现_read方法
+    _read (size) {
+        while(this.count--){
+            let buf=Buffer.from([this.rand(), this.rand(), this.rand(), this.rand()]);
+            // console.log(`size: ${buf.length} ${buf.readInt32BE(0)}`)  //大端，从从左到右读取
+            // console.log( JSON.stringify(buf))
+            this.push(buf, 'utf8')          // 向缓存中添加数据
+        }
+        this.push(null)
+    }
+    rand(){
+        return ~~(Math.random()*255)
+    }
+}
+
+let randomStream = new RandomStream({count:20})
+// 22500000
+randomStream.on('data', (chunk) => {
+    console.log(`read ${chunk}`)
+}).on('end', _ => console.log('end '))
