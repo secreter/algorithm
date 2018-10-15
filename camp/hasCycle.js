dd4bc68c82804d7eb8531e975088d2e75a45c65c
secreter <1979510177@qq.com>
2018-10-15 18:09:57 +0800

    hasCycle

--- camp/hasCycle.js
+++ camp/hasCycle.js
@@ -1,6 +1,21 @@
 /*
 * 写一个函数has_cycle判断任何一个对象是否有循环引用的情况。
 * */
-function hasCycle () {
-  
+function hasCycle(node,next=(node)=>node.next){
+  let slow=node,fast=node
+  while(slow&&fast){
+    slow=next(slow)
+    if(!next(fast)||!next(next(fast)))
+      break
+    fast=next(next(fast))
+    if(slow===fast) return true
+  }
+  return false
 }
+let node1={}
+let node2={child:node1}
+let node3={child:node2}
+let node4={child:node3}
+let node5={child:node4}
+node1.child=node3
+console.log(hasCycle(node5,node=>node.child))
